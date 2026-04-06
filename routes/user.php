e1d338cab2c4278a48f4fcb355e0261f7bf3f2d6
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-04-06 07:50:07 +0600

    Update user.php

--- routes/user.php
+++ routes/user.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\Backend\User\UserDashboardController;
 use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
-
-    // Profile Routes
-    Route::get('/profile', [UserProfileController::class, 'edit'])->name('user-profile.edit');
-    Route::post('/profile', [UserProfileController::class, 'update'])->name('user-profile.update');
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');  
 });
